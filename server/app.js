101e55028f74f82eca5726e05de28a3251c5ea65
Phil Kalinowski (RIT Student) <pak5559@rit.edu>
2022-02-28 14:36:12 -0500

    changes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 const router = require('./router.js');
 
@@ -17,6 +18,14 @@ app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// tell app to use this library
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: '',
+}));
+
+app.set('view engine', 'handlebars'); // handlebars that I want you to use
+app.set('views', `${__dirname}/../views`);// filepath to where views exist, where to look
+
 router(app);
 
 app.listen(port, (err) => {
@@ -25,4 +34,3 @@ app.listen(port, (err) => {
   }
   console.log(`Listening on port ${port}`);
 });
-
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,29 +1,49 @@
 // There is no reason for the name here except as an
 // example of how to set something for the POST
-let name = 'unknown';
+let name = [];
 
 const hostIndex = (req, res) => {
+  res.render('index', {
+    title: 'Home',
+    pageName: 'Home Page',
+    userName: name,
+  }); //what view do I want this to load?
+  //anything that ends in .handlebars is a view template
 
+  //title is considered a variable, and the value is replaced in each variable called title
 };
 
 const hostPage1 = (req, res) => {
-
+  return res.render('page1'),{
+    title: 'Page 1',
+    names: ['philip, batman, allison'],
+  };
 };
 
 const hostPage2 = (req, res) => {
-
+  return res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  return res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname){
+    return res.status(400).json({
+      error: 'firstname and lastname are both required',
+      id: 'setNameMissingParams',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  return res.json({name});
 };
 
 const notFound = (req, res) => {
-
+  return res.render('notFound', {
+    page: req.url,
+  })
 };
 
 module.exports = {
